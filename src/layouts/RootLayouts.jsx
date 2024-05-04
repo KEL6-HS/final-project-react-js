f004fa661387dff8df2e0d7a5ce529a8b8cb199e
RasyidMF <udangayam660@gmail.com>
2024-05-04 20:36:35 +0800

    Fixing bad script

--- src/layouts/RootLayouts.jsx
+++ src/layouts/RootLayouts.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Navbar from '../components/organisems/Navbar'
-import Footer from '../components/organisems/Footer'
+import React, { useEffect } from "react";
+import Navbar from "../components/organisems/Navbar";
+import Footer from "../components/organisems/Footer";
+import mainFunc from "../assets/main";
 
 export default function RootLayouts({ children }) {
-  return (
-   <>
-    <Navbar/>
-    {children}
-    <Footer/>
-   </>
-  )
+	useEffect(() => {
+		mainFunc(jQuery);
+	});
+
+	return (
+		<>
+			<Navbar />
+			{children}
+			<Footer />
+		</>
+	);
 }
